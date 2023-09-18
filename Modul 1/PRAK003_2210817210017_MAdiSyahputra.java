5a093ffaaca0e4be9e4ca78a5c06d0e5e7e26b5b
MAdiSyahputra <114968207+MAdiSyahputra@users.noreply.github.com>
2023-09-18 22:22:45 +0800

    Add files via upload

---
+++ Modul 1/PRAK003_2210817210017_MAdiSyahputra.java
@@ -0,0 +1,12 @@
+public class PRAK003_2210817210017_MAdiSyahputra {
+     public static void main(String[] args) {
+        System.out.println("     #########        ##   ");
+        System.out.println("    #         #       ##   ");
+        System.out.println("   #           #      ##   ");
+        System.out.println("  ###############     ##   ");
+        System.out.println(" #              #     ##   ");
+        System.out.println(" #              #     ##   ");
+        System.out.println(" #              #     ##   ");
+    }
+    }
+
