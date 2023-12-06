3c9d0fd891ad4c04013c8cb81ba0ec3256fe355f
MAdiSyahputra <114968207+MAdiSyahputra@users.noreply.github.com>
2023-12-06 16:58:40 +0800

    Add files via upload

---
+++ PRAKTIKUM0/src/PRAK001_2210817210017_MAdiSyahputra.java
@@ -0,0 +1,7 @@
+public class PRAK001_2210817210017_MAdiSyahputra {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi, M. Adi Syahputra");
+        System.out.println("Selamat Siang, M. Adi Syahputra");
+        System.out.println("Selamat Malam, M. Adi Syahputra");
+    }
+}
