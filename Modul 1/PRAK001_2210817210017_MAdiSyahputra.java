e86d2dd587b17d70ea792751bdd49ee03091414c
MAdiSyahputra <114968207+MAdiSyahputra@users.noreply.github.com>
2023-12-06 16:57:49 +0800

    Delete Modul 1 directory

--- Modul 1/PRAK001_2210817210017_MAdiSyahputra.java
+++
@@ -1,7 +0,0 @@
-public class PRAK001_2210817210017_MAdiSyahputra {
-    public static void main(String[] args) {
-        System.out.println("Selamat Pagi, M. Adi Syahputra");
-        System.out.println("Selamat Siang, M. Adi Syahputra");
-        System.out.println("Selamat Malam, M. Adi Syahputra");
-    }
-}
--- Modul 1/PRAK004_2210817210017_MAdiSyahputra.java
+++
@@ -1,6 +0,0 @@
-public class PRAK004_2210817210017_MAdiSyahputra {
-    public static void main(String[] args) {
-        String Nama = "M. Adi Syahputra"; Integer Umur = 19;
-        System.out.println("Perkenalkan, Nama Saya " + Nama + ", Umur Saya " + Umur );
-    }
-    }
